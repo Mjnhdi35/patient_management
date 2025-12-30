9347b6cee245321bdb2f7835de29aed210665bd2
thienb <mjnhdi35@gmail.com>
2025-12-30 17:58:05 +0700

    setup prettier for root monorepo and init api

--- infra/scripts/down-local.sh
+++ infra/scripts/down-local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-docker compose -f infra/docker/adminer/docker-compose.yml down
-docker compose -f infra/docker/redis/docker-compose.yml down
-docker compose -f infra/docker/postgres/docker-compose.yml down
+docker compose -f infra/docker/adminer/docker-compose.yml down || true
+docker compose -f infra/docker/redis/docker-compose.yml down || true
+docker compose -f infra/docker/postgres/docker-compose.yml down || true
 
 echo "Infra local is down"
